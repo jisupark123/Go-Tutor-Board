8536ea8ad267d9f9f598930a493e414f9b2f261b
박지수 <jisupark2000@gmail.com>
2026-06-29 19:59:18 +0900

    chore: 참고용 temp 디렉터리를 git·lint·타입체크에서 제외

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['dist', 'node_modules'],
+    ignores: ['dist', 'node_modules', 'temp'],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
